25fa4c244a0b4b574a6911a801f9b5fbb45a093b
Stone Pack <stone.pack@outlook.com>
2021-06-28 12:35:51 -0700

    removed boilerplate comment

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,6 @@ void main() {
 }
 
 class App extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
