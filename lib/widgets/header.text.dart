f77cf462e4948dbad5030ef6ef61f6a1f4bf5656
Stone Pack <stone.pack@outlook.com>
2021-07-23 21:25:37 -0700

    separated header text widget

---
+++ lib/widgets/header.text.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class HeaderText extends StatelessWidget {
+  const HeaderText({Key? key, required this.text}) : super(key: key);
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: TextStyle(
+        color: Colors.black,
+        fontSize: 40,
+      ),
+    );
+  }
+}
